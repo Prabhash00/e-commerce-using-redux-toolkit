be9fb194b3ee2d48c3fbe824c5ae348672f3661f
Prabhash Singh <prabhash.amazon.lko@gmail.com>
2025-05-13 22:03:30 +0530

    updates

--- src/components/mainPages/MainFile.jsx
+++ src/components/mainPages/MainFile.jsx
@@ -2,10 +2,16 @@ import { useEffect } from "react";
 import { fetchData } from "../../redux/slice/productSlice";
 import "./mainFile.css";
 import { useDispatch, useSelector } from "react-redux";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MainFile() {
   const dispatch = useDispatch();
-  const { isLoading, data } = useSelector((state) => state.ecom);
+  const { isLoading, data } = useSelector((state) => state.products);
+  const images = [
+    "https://mobirise.com/extensions/commercem4/assets/images/galleries-1-1200x800.png",
+    "https://png.pngtree.com/thumb_back/fh260/background/20221218/pngtree-technology-and-multimedia-online-shopping-concept-sale-tv-buy-photo-image_43548278.jpg",
+  ];
 
   useEffect(() => {
     dispatch(fetchData());
@@ -15,8 +21,26 @@ function MainFile() {
 
   return (
     <>
-      <h3>Home Page</h3>
-      
+      <Carousel
+        useKeyboardArrows
+        autoPlay
+        interval={1500}
+        infiniteLoop
+        showThumbs={true}
+        showStatus={true}
+        className="w-full"
+      >
+        {images.map((URL, index) => (
+          <div key={index} className="w-full h-full">
+            <img
+              src={URL}
+              alt={`carousel-image-${index}`}
+              className="w-[auto] h-auto max-h-[950px] object-contain"
+            />
+          </div>
+        ))}
+      </Carousel>
+
       <ul>{data && data.map((item) => <li key={item.id}>{item.title}</li>)}</ul>
     </>
   );
